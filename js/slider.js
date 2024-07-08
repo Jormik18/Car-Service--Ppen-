cd3733c3cbfd61430acec374427d6ee8900d0592
roj <kimroj2004@gmail.com>
2024-07-08 15:53:52 +0800

    hi

---
+++ js/slider.js
@@ -0,0 +1,12 @@
+var slider = tns({
+  container: ".try__slider",
+  items: 3,
+  speed: 200,
+  gutter: 100,
+  sliderBy: "page",
+  autoplay: true,
+  // navPosition: "bottom",
+  // containerControls: "#controls",
+  // prevButton: ".prev",
+  // nextButton: ".next",
+});
